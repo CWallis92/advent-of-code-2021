d7ef439b16ed7bba3efafc27e61191e68eaf1453
Chris <chriswallis92@gmail.com>
2021-12-11 23:27:28 +0000

    Day one complete

--- day-1/solution.js
+++ day-1/solution.js
@@ -1,8 +1,23 @@
 const fs = require("fs");
 
-const text = fs.readFileSync(`${__dirname}/data.txt`, "utf8");
-console.log(text);
+const inputs = fs.readFileSync(`${__dirname}/data.txt`, "utf8").split("\n");
 
-const textByLine = text.split("\n");
+const increases = inputs.reduce((acc, curr, index, array) => {
+  if (parseInt(curr) > parseInt(array[index - 1])) acc = acc + 1;
+  return acc;
+}, 0);
 
-console.log(textByLine);
+console.log(increases);
+
+const partTwo = inputs.reduce((acc, curr, index, array) => {
+  const prevSum =
+    parseInt(array[index - 1]) +
+    parseInt(array[index - 2]) +
+    parseInt(array[index - 3]);
+  const currSum =
+    parseInt(curr) + parseInt(array[index - 1]) + parseInt(array[index - 2]);
+  if (currSum > prevSum) acc++;
+  return acc;
+}, 0);
+
+console.log(partTwo);
