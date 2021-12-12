d817658ca86ce781410cb3f7db2dbc25891b78b1
Chris <chriswallis92@gmail.com>
2021-12-12 20:07:38 +0000

    Day 4 finished

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 1. https://adventofcode.com/2021/day/1
 2. https://adventofcode.com/2021/day/2
 3. https://adventofcode.com/2021/day/3
+4. https://adventofcode.com/2021/day/4
+5. https://adventofcode.com/2021/day/5
--- scripts/day-4.js
+++ scripts/day-4.js
@@ -1,12 +1,5 @@
 const fs = require("fs");
 
-const data = fs
-  // .readFileSync(`${__dirname}/../data/day-4.txt`, "utf8")
-  .readFileSync(`${__dirname}/../tests/day-4.txt`, "utf8")
-  .split("\n\n");
-
-const inputs = data.shift().split(",");
-
 class Board {
   constructor(grid) {
     this.flatGrid = [].concat(...grid);
@@ -22,11 +15,11 @@ class Board {
 
   updateGrid(val) {
     if (this.flatGrid.indexOf(val) > -1) {
+      this.flatGrid.splice(this.flatGrid.indexOf(val), 1);
       const rowToShrink = this.rows.findIndex((row) => row.indexOf(val) > -1);
       const colToShrink = this.cols.findIndex((row) => row.indexOf(val) > -1);
       this.rows[rowToShrink].splice(this.rows[rowToShrink].indexOf(val), 1);
       this.cols[colToShrink].splice(this.cols[colToShrink].indexOf(val), 1);
-      this.flatGrid.splice(this.flatGrid.indexOf(val), 1);
       return this.checkWinner();
     }
     return false;
@@ -38,11 +31,17 @@ class Board {
       Math.min(...this.cols.map((col) => col.length)) === 0
     ) {
       this.won = true;
-      return true;
     }
-    return false;
+    return this.won;
   }
 }
+
+const data = fs
+  .readFileSync(`${__dirname}/../data/day-4.txt`, "utf8")
+  // .readFileSync(`${__dirname}/../tests/day-4.txt`, "utf8")
+  .split("\n\n");
+
+const inputs = data.shift().split(",");
 
 const grids = data.map((grid) => {
   const gridArray = grid
@@ -51,45 +50,35 @@ const grids = data.map((grid) => {
   return new Board(gridArray);
 });
 
-// Part 1
-let win = false,
-  calledNum = "",
+let complete = false,
+  win = false,
+  calledNumWin = "",
+  calledNumLoss = "",
   callIndex = 0,
-  winningGridSum = 0;
+  winningGridSum = 0,
+  losingGridSum = 0;
 
 while (callIndex < inputs.length) {
-  calledNum = inputs[callIndex];
-  grids.forEach((grid) => {
-    if (grid.updateGrid(calledNum)) {
-      winningGridSum = grid.flatGrid.reduce(
+  calledNumLoss = inputs[callIndex];
+  grids.forEach((currGrid) => {
+    if (currGrid.updateGrid(calledNumLoss) && !win) {
+      calledNumWin = calledNumLoss;
+      winningGridSum = currGrid.flatGrid.reduce(
         (acc, curr) => parseInt(acc) + parseInt(curr)
       );
       win = true;
     }
+    if (!complete && grids.every((grid) => grid.won)) {
+      losingGridSum = currGrid.flatGrid.reduce(
+        (acc, curr) => parseInt(acc) + parseInt(curr)
+      );
+      complete = true;
+    }
   });
-  if (win) break;
+  if (complete) break;
   callIndex++;
 }
 
-console.log("Part 1:", winningGridSum * parseInt(calledNum));
+console.log("Part 1:", winningGridSum * parseInt(calledNumWin));
 
-// Part 2
-//   calledNum = "",
-//   callIndex = 0,
-//   winningGridSum = 0;
-
-// while (callIndex < inputs.length) {
-//   calledNum = inputs[callIndex];
-//   grids.forEach((grid) => {
-//     if (grid.updateGrid(calledNum)) {
-//       winningGridSum = grid.flatGrid.reduce(
-//         (acc, curr) => parseInt(acc) + parseInt(curr)
-//       );
-//       win = true;
-//     }
-//   });
-//   if (win) break;
-//   callIndex++;
-// }
-
-console.log("Part 2:");
+console.log("Part 2:", losingGridSum * parseInt(calledNumLoss));
