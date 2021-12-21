657d227fe2c958272cd8cdc3b0d2d0ebab6a6a7a
Chris <chriswallis92@gmail.com>
2021-12-21 18:53:55 +0000

    Day 14 finished

--- scripts/day-14.js
+++ scripts/day-14.js
@@ -3,8 +3,8 @@ const fs = require("fs");
 const data = fs
   .readFileSync(
     `${__dirname}/../` +
-      "testData" +
-      // "data" +
+      // "testData" +
+      "data" +
       `/day-${__filename.split("/day-")[1].split(".")[0]}.txt`,
     "utf8"
   )
@@ -12,61 +12,57 @@ const data = fs
 
 let out = data[0];
 
-// const template = data.slice(2).map((input) => input.split(" -> "));
-
-// const letters = [...new Set(template.flatMap((rule) => rule[0].split("")))];
-
-// for (let step = 1; step <= 10; step++) {
-//   let pos = 0;
-
-//   while (pos < out.length - 1) {
-//     const ruleIndex = template.findIndex(
-//       (item) => item[0] === out.substring(pos, pos + 2)
-//     );
-//     out =
-//       out.substring(0, pos + 1) +
-//       template[ruleIndex][1] +
-//       out.substring(pos + 1);
-//     pos += 2;
-//   }
-// }
-
-// const occurences = letters
-//   .map((letter) => {
-//     const re = new RegExp(letter, "g");
-//     return out.match(re).length;
-//   })
-//   .sort((a, b) => b - a);
-
-// console.log("Part 1:", occurences[0] - occurences[occurences.length - 1]);
-
-// Part 2
 const template = data.slice(2).map((input) => input.split(" -> "));
 
+//Pairs are always replaced in the string by 2 other pairs, as defined by the rules
 const rules = {};
 for (const rule of template) {
   rules[rule[0]] = rule[1];
 }
 
-const letters = [...new Set(template.flatMap((rule) => rule[0].split("")))];
-const total = {};
-for (const letter of letters) {
+// Letters are never removed from the string, so we never need to recount
+const letterCounts = {};
+for (const letter of [
+  ...new Set(template.flatMap((rule) => rule[0].split(""))),
+]) {
   const re = new RegExp(letter, "g");
-  total[letter] = out.match(re) ? out.match(re).length : 0;
+  const matches = out.match(re);
+  letterCounts[letter] = matches ? matches.length : 0;
 }
 
+// Pairs, however, are replaced. We iterate through the oriinal pairs to work out the new ones
 const pairCounts = {};
 for (pair of Object.keys(rules)) {
   pairCounts[pair] = 0;
 }
-
 for (let i = 0; i < out.length - 1; i++) {
   pairCounts[out.substring(i, i + 2)] += 1;
 }
 
-for (let step = 1; step <= 10; step++) {
-  const currPairCounts = JSON.parse(JSON.stringify(pairCounts));
-  // for (let x )
-}
+const answer = (steps) => {
+  let pairCountsClone = JSON.parse(JSON.stringify(pairCounts));
+  const letterCountsClone = JSON.parse(JSON.stringify(letterCounts));
 
-console.log(total);
+  for (let step = 1; step <= steps; step++) {
+    const newPairCounts = {};
+    for (pair of Object.keys(rules)) {
+      newPairCounts[pair] = 0;
+    }
+
+    for (const pair of Object.keys(pairCounts)) {
+      newPairCounts[pair[0] + rules[pair]] += pairCountsClone[pair];
+      newPairCounts[rules[pair] + pair[1]] += pairCountsClone[pair];
+      letterCountsClone[rules[pair]] += pairCountsClone[pair];
+    }
+
+    pairCountsClone = newPairCounts;
+  }
+
+  const counts = Object.values(letterCountsClone).sort((a, b) => b - a);
+
+  return counts[0] - counts[counts.length - 1];
+};
+
+console.log("Part 1:", answer(10));
+
+console.log("Part 2:", answer(40));
