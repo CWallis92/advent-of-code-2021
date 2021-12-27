90363473af40b87de38031ab8775ab09abb5a643
Chris <chriswallis92@gmail.com>
2021-12-27 23:36:40 +0000

    Day 23 started

---
+++ scripts/day-23.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+
+const data = fs
+  .readFileSync(
+    `${__dirname}/../` +
+      // "testData" +
+      "data" +
+      `/day-${__filename.split("/day-")[1].split(".")[0]}.txt`,
+    "utf8"
+  )
+  .split("\n");
